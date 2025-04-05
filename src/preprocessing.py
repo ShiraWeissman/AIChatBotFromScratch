73feac52ca302080f157093e817c80df36842830
ShiraWeissman <shira.va@gmail.com>
2025-04-05 19:00:14 +0300

    adding dataset["test"]

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -117,13 +117,16 @@ def load_and_preprocess_dataset(dataset_name, model_type="distilgpt2", sample_si
         dataset = load_dataset(dataset_path,  "unfiltered", trust_remote_code=True)
     elif dataset_name == 'wikipedia':
         dataset = load_dataset(dataset_path, "20231101.en", trust_remote_code=True)
-    split_idx = int(0.1 * sample_size)
+    valid_split_idx = int(0.1 * sample_size)
+    test_split_idx = int(0.01 * sample_size)
     if len(dataset['train']) > sample_size:
         dataset["train"] = dataset["train"].select(range(sample_size))
-        dataset["validation"] = dataset["train"].select(range(split_idx))
-        dataset["train"] = dataset["train"].select(range(split_idx, sample_size))
+        dataset["test"] = dataset["train"].select(range(test_split_idx))
+        dataset["validation"] = dataset["train"].select(range(test_split_idx, valid_split_idx))
+        dataset["train"] = dataset["train"].select(range(valid_split_idx, sample_size))
         print(len(dataset['train']))
         print(len(dataset['validation']))
+        print(len(dataset['test']))
 
     print(f"Preprocessing {dataset_name} for {model_type} model...")
     dataset = dataset.map(lambda x: preprocess_text(x, model_type=model_type, dataset_name=dataset_name))
